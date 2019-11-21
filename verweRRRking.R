0c039587e495d7dc319107a5fb0b323e9d502f10
TunaTerror4 <Rens.Dijkhuizen@gmail.com>
2019-11-21 11:20:12 +0100

    Basten version

--- verweRRRking.R
+++ verweRRRking.R
@@ -1,39 +1,49 @@
-#' ---
-#'  
-#' # Introductie
-#' 
-#' Planten bestaan enzo
-
-#'  
-#' # Beschrijvende statistiek
-#'  
-
-#' De dataset ziet er als volgt uit
-#' 
 library(rjson)
 library(stringr)
 
-#' setting workdirectory
-setwd("C:/Users/RensD/OneDrive/studie/Master/The_big_project/output")
-top_files <- Sys.glob("*top_results.txt")
-lijstje <- rep(NA, length(top_files))
-i <- 1
-for(file in top_files){
-  resultsfile <- fromJSON(file = file)
-  data <- resultsfile$observations
-  GT <- str_extract(file, "- [0-9]*_cam")
-  GT <- substring(GT, 3, 4)
-  data$GT <- GT
-  append(lijstje, data, after = i)
-  i <- i + 1
+## load one file
+
+setwd("~/studie/master/data")
+tmp <- fromJSON(file="output_lab/0200_2018-03-07 08.55 - 26_cam9top_results.txt")
+tmp
+
+## test see what is in data file
+tmp$observations$horizontal_reference_position
+tmp$observations$height_above_reference
+tmp$observations$hue_circular_mean
+tmp$observations$green_frequencies
+tmp$observations$solidity$value
+tmp$observations[[1]]$value
+length(tmp$observations)
+
+## make trait list 
+trait.list <- NULL
+for (i in 1:length(tmp$observations)){
+  trait.list <- c(trait.list,rep(tmp$observations[[i]]$trait,length(tmp$observations[[i]]$value)))
+                  }
+trait.list
+
+## get data
+file.list <- list.files("output_lab/")
+file.list <- file.list[grep("results.txt",file.list)]
+file.list
+
+pheno.collect <- matrix(NA,nrow = length(file.list),ncol = length(trait.list))
+for ( j in 1:length(file.list)){
+tmp <- fromJSON(file=paste0(c("output_lab/",file.list[j]),collapse = ""))
+pheno.tmp <- NULL
+GT <- str_extract(file.list[j], "- [0-9]*_cam")
+GT <- substring(GT, 3, 4)
+for (i in 1:length(tmp$observations)){
+  pheno.tmp <- c(pheno.tmp,tmp$observations[[i]]$value)
 }
+pheno.collect[j,] <- pheno.tmp
+}
+colnames(pheno.collect) <- trait.list
+rownames(pheno.collect) <- file.list
+pheno.collect[1:5,1:5]
 
-print(data$GT)
-#do.call(rbind, Map(data.frame, A = data, B=data))
-
-
-#frame <- data.frame("number", "genotype", "observations")
-
-
-
+pheno.collect <- data.frame(pheno.collect)
+plot(pheno.collect$area.above.reference)
+plot(pheno.collect$solidity)
 
