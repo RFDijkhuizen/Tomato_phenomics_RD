cb50ed073b032cceada1eafc273a2c0984314fcd
TunaTerror4 <Rens.Dijkhuizen@gmail.com>
2019-11-22 12:30:59 +0100

    Now outputs genotype
    Added regex to get genotype and output it in json file

--- VIS_workflow.py
+++ VIS_workflow.py
@@ -8,6 +8,7 @@ from plantcv import plantcv as pcv
 import os
 import glob
 from PIL import Image
+import re
 
 class args:
     image = ""
@@ -33,11 +34,13 @@ a_lower_thresh_1 = 118              # 118 Lower thresh to be considered plant
 a_lower_thresh_2 = 138              # 138 Lower thresh to be considered too magenta to be plant
 b_lower_thresh_1 = 165              # 165 Threshold to filter background away
 b_higher_thresh_1 = 245             # 245 Threshold to filter background away
-b_fill_k = 1000                     # 1000 Fill to make sure we do not lose anything
 b_lower_thresh_2 = 120              # 120 Threshold to capture plant
 b_higher_thresh_2 = 255             # 255 Threshold to capture plant
+b_fill_k = 1000                     # 1000 Fill to make sure we do not lose anything
 LAB_fill_k = 1500                   # 1500 Fill kernel for the LAB filtered image
 LAB_blur_k = 10                     # 10 Final Blur
+pattern = ".*- (\d+)\w+\d+.png"     # Pattern to get your genotype from filename
+replacement = "\g<1>"               # Replacement regex to get your genotype
 ### Main workflow
 def main():
     pcv.params.debug = args.debug  # set debug mode
@@ -108,33 +111,33 @@ def main():
     ab1 = pcv.logical_and(bin_img1=maskeda_thresh, bin_img2=maskedb_thresh)
     ab = pcv.logical_and(bin_img1=maskedl_thresh, bin_img2=ab1)
 
-
-    # Fill small objects
-    ab_fill = pcv.median_blur(gray_img=ab, ksize= LAB_blur_k)
-    ab_fill = pcv.fill(bin_img=ab_fill, size=LAB_fill_k)
-    print("final fill and blur")
-    # Apply mask (for VIS images, mask_color=white)
-    masked2 = pcv.apply_mask(rgb_img=masked, mask=ab_fill, mask_color='white')
-
-    # Identify objects
-    id_objects, obj_hierarchy = pcv.find_objects(img=masked2, mask=ab_fill)
-
-    # Define ROI
-    roi1, roi_hierarchy= pcv.roi.rectangle(img=masked2, x=0, y=0, h=960, w=1280)
-
-    # Decide which objects to keep
-    roi_objects, hierarchy3, kept_mask, obj_area = pcv.roi_objects(img=img, roi_contour=roi1, 
-                                                               roi_hierarchy=roi_hierarchy, 
-                                                               object_contour=id_objects, 
-                                                               obj_hierarchy=obj_hierarchy,
-                                                               roi_type='partial')
-
-        # Object combine kept objects
-    obj, mask = pcv.object_composition(img=img, contours=roi_objects, hierarchy=hierarchy3)
-        ############### Analysis ################
     try:
+        # Fill small objects
+        ab_fill = pcv.median_blur(gray_img=ab, ksize= LAB_blur_k)
+        ab_fill = pcv.fill(bin_img=ab_fill, size=LAB_fill_k)
+        print("final fill and blur")
+        # Apply mask (for VIS images, mask_color=white)
+        masked2 = pcv.apply_mask(rgb_img=masked, mask=ab_fill, mask_color='white')
+    
+        # Identify objects
+        id_objects, obj_hierarchy = pcv.find_objects(img=masked2, mask=ab_fill)
+    
+        # Define ROI
+        roi1, roi_hierarchy= pcv.roi.rectangle(img=masked2, x=0, y=0, h=960, w=1280)
+    
+        # Decide which objects to keep
+        roi_objects, hierarchy3, kept_mask, obj_area = pcv.roi_objects(img=img, roi_contour=roi1, 
+                                                                   roi_hierarchy=roi_hierarchy, 
+                                                                   object_contour=id_objects, 
+                                                                   obj_hierarchy=obj_hierarchy,
+                                                                   roi_type='partial')
+    
+            # Object combine kept objects
+        obj, mask = pcv.object_composition(img=img, contours=roi_objects, hierarchy=hierarchy3)
+            ############### Analysis ################
+        
         outfile=args.outdir+"/"+filename
-        # Pseudocolor the grayscale image
+            # Pseudocolor the grayscale image
         pseudocolored_img = pcv.visualize.pseudocolor(gray_img=s, mask=kept_mask, cmap='jet')
         
         # Shape properties relative to user boundary line (optional)
@@ -150,12 +153,18 @@ def main():
         color_histogram = pcv.analyze_color(rgb_img=img, mask=kept_mask, hist_plot_type='all')
         
         # Find shape properties, output shape image (optional)
+
         shape_img = pcv.analyze_object(img=img, obj=obj, mask=mask)
         new_im = Image.fromarray(shape_img)
         new_im.save("output//" + args.filename + "shape_img.png")
-    
+        GT = re.sub(pattern, replacement, filename)
+        pcv.outputs.add_observation(variable = "Genotype", trait = "The genotype",
+                                    method = "Regexed from the filename", scale = None,
+                                    datatype = str, value = GT, label = "Genotype")
+        
         # Write shape and color data to results file
         pcv.print_results(filename=args.result)
+
     except:
         print("not enough plant material found")
 
@@ -178,7 +187,7 @@ if do_subset == True:
         side_files.append(os.path.join(wd, item))
         
     file_counter = 0
-    for item in top_files:
+    for item in top_files[0:1]:
         args.image = item
         args.debug = "plot"
         args.outdir = "C:\\Users\\RensD\\OneDrive\\studie\\Master\\The_big_project\\subset\\"
@@ -192,6 +201,7 @@ if do_subset == True:
 do_all = False
 if do_all == True:      
     wd = os.getcwd()
+    args.debug = "None"
     top_files = []          # absolute paths uses for processing
     top_files_names = []    # The names used for storing 
     temp = glob.glob("top_input//*cam9.png")
@@ -206,7 +216,7 @@ if do_all == True:
         side_files.append(os.path.join(wd, item))
         
     file_counter = 0
-    for item in top_files:
+    for item in top_files[0:25]:
         args.image = item
         args.outdir = "C:\\Users\\RensD\\OneDrive\\studie\\Master\\The_big_project\\output\\"
         args.result = "output//" + top_files_names[file_counter][0:-4] + "top_results.txt"
