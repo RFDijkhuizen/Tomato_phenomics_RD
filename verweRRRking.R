6ddd94b76bfe977921da2897724c76bcff7c0fdb
TunaTerror4 <Rens.Dijkhuizen@gmail.com>
2019-11-25 12:05:17 +0100

    Now opens first file of file_list
    Now opens first file of file_list instead of manually inserting a path.

--- verweRRRking.R
+++ verweRRRking.R
@@ -2,38 +2,26 @@ library(rjson)
 library(stringr)
 
 ## load one file
+setwd("~/studie/master/data/")
 
-setwd("~/studie/master/data")
-tmp <- fromJSON(file="output_lab/0200_2018-03-07 08.55 - 26_cam9top_results.txt")
-tmp
+## get data
+file.list <- list.files("output_lab_ngt/")
+file.list <- file.list[grep("results.txt",file.list)]
 
-## test see what is in data file
-tmp$observations$horizontal_reference_position
-tmp$observations$height_above_reference
-tmp$observations$hue_circular_mean
-tmp$observations$green_frequencies
-tmp$observations$solidity$value
-tmp$observations[[1]]$value
-length(tmp$observations)
+## open one temp file
+tmp <- fromJSON(file=paste0(c("output_lab_ngt/",file.list[1]),collapse = ""))
 
-## make trait list 
 trait.list <- NULL
 for (i in 1:length(tmp$observations)){
   trait.list <- c(trait.list,rep(tmp$observations[[i]]$trait,length(tmp$observations[[i]]$value)))
-                  }
-trait.list
+}
+tmp$observations$genotype$value
 
-## get data
-file.list <- list.files("output_lab/")
-file.list <- file.list[grep("results.txt",file.list)]
-file.list
 
 pheno.collect <- matrix(NA,nrow = length(file.list),ncol = length(trait.list))
 for ( j in 1:length(file.list)){
-tmp <- fromJSON(file=paste0(c("output_lab/",file.list[j]),collapse = ""))
+tmp <- fromJSON(file=paste0(c("output_lab_ngt/",file.list[j]),collapse = ""))
 pheno.tmp <- NULL
-GT <- str_extract(file.list[j], "- [0-9]*_cam")
-GT <- substring(GT, 3, 4)
 for (i in 1:length(tmp$observations)){
   pheno.tmp <- c(pheno.tmp,tmp$observations[[i]]$value)
 }
@@ -42,8 +30,7 @@ pheno.collect[j,] <- pheno.tmp
 colnames(pheno.collect) <- trait.list
 rownames(pheno.collect) <- file.list
 pheno.collect[1:5,1:5]
-
 pheno.collect <- data.frame(pheno.collect)
-plot(pheno.collect$area.above.reference)
-plot(pheno.collect$solidity)
 
+plot(pheno.collect$area.above.reference)
+plot(pheno.collect$area)
