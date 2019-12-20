7400b76d19235bcf7cd8360e346fd25bc85e00bb
TunaTerror4 <Rens.Dijkhuizen@gmail.com>
2019-12-20 14:45:39 +0100

    Not_sure_what_I_did

--- verweRRRking.R
+++ verweRRRking.R
@@ -14,7 +14,7 @@ library(alphashape3d)
 #' In the end I probably only want to use color from these.
 setwd("~/rens2")
 
-## get data
+## get  data
 file.list <- list.files("output/")
 file.list <- file.list[grep("cam9top_results.txt",file.list)]
 
@@ -36,7 +36,6 @@ for (i in 1:length(tmp$observations)){
   }
   else{
     pheno.tmp <- c(pheno.tmp,tmp$observations[[i]]$value)
-      
   }
 }
 pheno.collect[j,] <- pheno.tmp
@@ -44,15 +43,14 @@ pheno.collect[j,] <- pheno.tmp
 colnames(pheno.collect) <- trait.list
 rownames(pheno.collect) <- file.list
 pheno.collect[1:5,1:5]
-pheno.collect <- data.frame(pheno.collect)
+pheno.collect <- data.frame(pheno.collect, stringsAsFactors = FALSE )
 pheno.collect$genotype <- as.factor(pheno.collect$genotype)
 pheno.collect$whether.the.plant.goes.out.of.bounds. <- as.factor(pheno.collect$whether.the.plant.goes.out.of.bounds.)
 pheno.collect$object.in.frame <- as.factor(pheno.collect$object.in.frame)
 #' random nice plots.
-#' The plots dont really work at the moment because ~ genotype doesnt work now that the genotype
-#' is a string and not a number (I think)
+#' The plots dont really work at the moment because all my values have become strings.
 plot(pheno.collect$area)
-ggplot(pheno.collect, aes(x = object.in.frame)) + geom_boxplot()
+ggplot(pheno.collect, aes(x = object.in.frame)) + geom_bar()
 ggplot(pheno.collect, aes(x = genotype)) + geom_bar()
 ggplot(pheno.collect[1:10,1:36], aes(y = area, x= genotype)) + geom_boxplot()
 small_subset <- subset(pheno.collect, genotype == "207A" | genotype == "271A" | genotype == "278A" | genotype == "283A")
@@ -83,12 +81,16 @@ for ( j in 1:length(file.list)){
   tmp <- fromJSON(file=paste0(c("output/",file.list[j]),collapse = ""))
   pheno.tmp <- NULL
   for (i in 1:length(tmp$observations)){
-    print(length(mean(tmp$observations[[i]]$value)))
-    pheno.tmp <- c(pheno.tmp,mean(tmp$observations[[i]]$value))
-
+    if (length(tmp$observations[[i]]$value) > 1){
+      pheno.tmp <- c(pheno.tmp,mean(tmp$observations[[i]]$value))
+    }
+    else{
+      pheno.tmp <- c(pheno.tmp,tmp$observations[[i]]$value)
+    }
   }
   pheno.collect[j,] <- pheno.tmp
 }
+
 colnames(pheno.collect) <- trait.list
 rownames(pheno.collect) <- file.list
 pheno.collect[1:5,1:5]
@@ -97,7 +99,8 @@ pheno.collect <- data.frame(pheno.collect)
 #' RANDOM NICE PLOTS
 plot(pheno.collect$area.above.reference)
 plot(pheno.collect$area)
-
+ggplot(pheno.collect, aes(x = object.in.frame)) + geom_bar()
+ggplot(pheno.collect, aes(x = genotype)) + geom_bar()
 boxplot(pheno.collect$convex.hull.area ~  pheno.collect$genotype)
 boxplot(pheno.collect$perimeter ~  pheno.collect$genotype)
 boxplot(pheno.collect$longest.path ~  pheno.collect$genotype)
@@ -124,7 +127,6 @@ trait.list <- NULL
 for (i in 1:length(tmp$observations)){
   trait.list <- c(trait.list,rep(tmp$observations[[i]]$trait,1))
 }
-tmp$observations$genotype$value
 
 pheno.collect <- matrix(NA,nrow = length(file.list),ncol = length(trait.list))
 for ( j in 1:length(file.list)){
@@ -144,7 +146,8 @@ pheno.collect <- data.frame(pheno.collect)
 #' RANDOM NICE PLOTS
 plot(pheno.collect$area.above.reference)
 plot(pheno.collect$area)
-
+ggplot(pheno.collect, aes(x = object.in.frame)) + geom_bar()
+ggplot(pheno.collect, aes(x = genotype)) + geom_bar()
 boxplot(area ~ genotype, data= pheno.collect)
 boxplot(solidity ~ genotype, data= pheno.collect)
 boxplot(pheno.collect$longest.path ~ pheno.collect$genotype)
@@ -196,7 +199,6 @@ qplot(geom = 'boxplot',genotype, area, data = pheno.collect)
 #' the general position assumption is not met (which is correct). But I am not sure this
 #' is the cause of the volume calculation error because I don't see how that is logic.
 punten <- read.csv("~/rens2/input/0030_2018-03-14 14.21 - 238A_0_3D.csv", header=FALSE)
-
 
 View(punten)
 x <- cbind(punten[,1], punten[,2], punten[,3])
